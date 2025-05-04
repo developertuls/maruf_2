6f584f36c1041c40b3ca1db090471c2d68cfc49a
Your Name <you@example.com>
2025-05-04 16:12:52 +0600

    okkk

--- custom.js
+++ custom.js
@@ -1,48 +1,64 @@
 
+const onn=() =>{
+document.getElementById("ss");
+const mm=ss
+mm.style.background="#6D214F";
+// mm.style.color="#000";
+mm.style.padding="3px";
+mm.style.borderRadius="5px";
+
+document.getElementById("text");
+const bgb=text
+bgb.style.visibility="visible"
+
+document.getElementById("text2");
+text2.style.background="#6D214F"
+text2.style.color="#fff"
+
+
+};
+
+// let marufIndex=0;
+
+
+// const showsliders=(index)=>{
+// document.getElementById("slides");
+// const total=slides.children.length
+// console.log(total)
+
+// if(index>=total){
+// marufIndex = 0;
+// }
+// else if(index<0){
+// marufIndex = total-1;
+// }
+// else{
+//   marufIndex=index;
+// }
+
+// slides.style.transform =`translateX(${-marufIndex * 100}%)`;
 
 
 
-let marufIndex=0;
-
-
-const showsliders=(index)=>{
-document.getElementById("slides");
-const total=slides.children.length
-console.log(total)
-
-if(index>=total){
-marufIndex = 0;
-}
-else if(index<0){
-marufIndex = total-1;
-}
-else{
-  marufIndex=index;
-}
-
-slides.style.transform =`translateX(${-marufIndex * 100}%)`;
 
 
 
 
 
-
-
-
-}
+// }
 
 
 
 
-const prevslide=() =>{
-  showsliders(marufIndex+1)
+// const prevslide=() =>{
+//   showsliders(marufIndex+1)
 
-}
-const nextslide =() =>{
-  showsliders( marufIndex-1)
+// }
+// const nextslide =() =>{
+//   showsliders( marufIndex-1)
   
 
-}
+// }
 
 // showsliders();
 
@@ -71,12 +87,12 @@ const nextslide =() =>{
 
 
 
-autosliding
-function autosliding(){
-  deletInterval = setInterval(timer,3000);
-  function timer(){
-    nextslide();
-  }
-}
-autosliding();
+// autosliding
+// function autosliding(){
+//   deletInterval = setInterval(timer,3000);
+//   function timer(){
+//     nextslide();
+//   }
+// }
+// autosliding();
 
